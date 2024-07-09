423d3b810d29fae22ca9291f0d115bbb9425864b
JitenderMX <jitender.work.mediax@gmail.com>
2024-07-09 15:34:25 +0530

    first commit

---
+++ src/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBcDwsFoGH_wBCuu2B4q3p3b3NWOrO2GxY",
+  authDomain: "happening-in-agra.firebaseapp.com",
+  projectId: "happening-in-agra",
+  storageBucket: "happening-in-agra.appspot.com",
+  messagingSenderId: "151723413804",
+  appId: "1:151723413804:web:50d97f3ae222a2e57f5c89"
+};
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+export const db = getFirestore()
