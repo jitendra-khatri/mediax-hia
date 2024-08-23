2a727832d34bad792100dbc69ce62fd6b3f7b2f4
sumeshk1 <sumesh1712@gmail.com>
2024-08-23 14:42:56 +0530

    feature - payment integration

---
+++ backend/index.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const Razorpay = require('razorpay');
+const cors = require('cors');
+const axios = require("axios");
+
+const app = express();
+const port = 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send("Hello World!")
+})
+
+
+
+// To generate the payment
+app.post('/orders', async(req,res)=> {
+    const razorpay = new Razorpay({
+        key_id: "rzp_test_P2P6lF14A7MpRI",
+        key_secret: "C6FoqNRzkLi1u5jAv1gPePFL"
+    })
+
+    const options = {
+        amount: req.body.amount,
+        currency: req.body.currency,
+        receipt: 'receipt#1',
+        payment_capture: 1
+    }
+
+    try{
+        const response = await razorpay.orders.create(options);
+
+        res.json({
+            order_id: response.id,
+            currency: response.currency,
+            amount: response.amount
+        })
+    } catch (error){
+        res.status(500).send(`Internal server error: ${error}`)
+    }
+
+})
+
+// to fetch the payment
+
+
+app.get("/payment/:paymentId", async(req, res)=>{
+    const {paymentId} = req.params;
+
+    const razorpay = new Razorpay({
+        key_id: "rzp_test_P2P6lF14A7MpRI",
+        key_secret: "C6FoqNRzkLi1u5jAv1gPePFL"
+    })
+
+    try {
+        const payment = await razorpay.payments.fetch(paymentId);
+
+        if(!payment){
+            return res.status(500).json("Error at razorpay loading")
+        }
+
+        res.json({
+            status: payment.status,
+            method: payment.method,
+            amount: payment.amount,
+            currency: payment.currency
+        })
+    }
+    catch(error){
+        res.status(500).send(`Internal server error: ${error}`)
+    }
+} )
+
+
+
+
+app.listen(port, ()=> {
+    console.log(`Server is running at ${port}`)
+})
+
--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -1,8 +1,125 @@
+import axios from "axios";
+import React from "react";
+import { useState } from "react";
+
 
 function Payment() {
+  const [responseId, setResponseId] = useState('');
+  const [responseState, setResponseState] = useState([]);
+
+  const loadScript = (src) => {
+    return new Promise((resolve)=>{
+      const script = document.createElement('script');
+
+      script.src = src;
+
+      script.onload = () => {
+        resolve(true)
+      }
+
+      script.onerror=()=>{
+        resolve(false)
+      }
+
+      document.body.appendChild(script)
+    })
+  }
+
+
+  const createRazorpayOrder = (amount) => {
+    let data = JSON.stringify({
+      amount: amount * 100,
+      currency: "INR"
+    })
+    console.log(data)
+
+    let config = {
+      method: "post",
+      maxBodyLength: Infinity,
+      url: "http://localhost:5000/orders",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data: data
+    }
+
+    axios.request(config)
+    .then((response)=> {
+      console.log(JSON.stringify(response.data))
+      handleRazorpayScreen(response.data.amount)
+    })
+    .catch((error)=>{
+      console.log('error at', error)
+    })
+
+    const handleRazorpayScreen = async(amount) => {
+      const res = await loadScript('http://checkout.razorpay.com/v1/checkout.js');
+      if(!res){
+        alert("Some error at razorpay screen loading");
+        return;
+      }
+
+      const options = {
+        key: 'rzp_test_P2P6lF14A7MpRI',
+        amount: amount,
+        currency: 'INR',
+        name: 'HIA',
+        description: 'payment to HIA',
+        image:"http://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg",
+        handler: function(response){
+          setResponseId(response.razorpay_payment_id)
+        },
+        prefill: {
+          name: "Happening In Agra",
+          email: "hello@mediax.co.in"
+        },
+        theme:{
+          color: "#1EAEBD"
+        }
+      }
+
+      const paymentObject = new window.Razorpay(options)
+      paymentObject.open()
+  }
+}
+
+
+
+  const paymentFetch = (e) => {
+    e.preventDefault();
+
+    const paymentId = e.target.paymentId.value;
+
+    axios.get(`http://localhost:5000/payment/${paymentId}`)
+    .then((response)=>{
+      console.log(response.data);
+      setResponseState(response.data)
+    })
+    .catch((error)=>{
+      console.log("error occures", error)
+    })
+  }
+
+
+
   return (
     <div>
       Payment
+      <button onClick={()=> createRazorpayOrder(100)} > Payment of 100 Rs.</button>
+      {responseId && <p>{responseId}</p>}
+      <h1> This is payment verification form </h1>
+      <form onSubmit={paymentFetch}>
+        <input type="text" name="paymentId" />
+        <button type="submit">Fetch Payment</button>
+        { responseState.length !==0 && (
+          <ul>
+            <li>Amount: {responseState.amount / 100} Rs. </li>
+            <li> Currency: {responseState.currency}</li>
+            <li>Status: {responseState.status}</li>
+            <li>Method: {responseState.method}</li>
+          </ul>
+        )}
+      </form>
     </div>
   )
 }
