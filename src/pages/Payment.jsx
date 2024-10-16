aba30b5d51e829926dba1f4e352bf70390106686
JitenderMX <jitender.work.mediax@gmail.com>
2024-10-16 15:49:28 +0530

    Cashfree tasting 3

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
-// import hiaLogo from '../assets/logo.png';
+import hiaLogo from '../assets/logo.png';
 import { getAuth } from 'firebase/auth';
 import { doc, getDoc, updateDoc } from 'firebase/firestore'
 import { db } from "../firebase.config";
@@ -99,7 +99,7 @@ function Payment() {
     paymentObject.open();
   }; */
 
-  let cashfree;
+  /*let cashfree;
 
   let insitialzeSDK = async function () {
 
@@ -167,10 +167,80 @@ function Payment() {
       console.log(error)
     }
 
-  }
+  } */
 
 
-   
+    let cashfree;
+
+    let insitialzeSDK = async function () {
+  
+      cashfree = await load({
+        mode: "sandbox",
+      })
+    }
+  
+    insitialzeSDK()
+  
+    const [orderId, setOrderId] = useState("")
+  
+  
+  
+    const getSessionId = async () => {
+      try {
+        let res = await axios.get("http://localhost:8000/payment")
+        
+        if(res.data && res.data.payment_session_id){
+  
+          console.log(res.data)
+          setOrderId(res.data.order_id)
+          return res.data.payment_session_id
+        }
+  
+  
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  
+    const verifyPayment = async () => {
+      try {
+        
+        let res = await axios.post("http://localhost:8000/verify", {
+          orderId: orderId
+        })
+  
+        if(res && res.data){
+          alert("payment verified")
+        }
+  
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  
+    const handleClick = async (e) => {
+      e.preventDefault()
+      try {
+  
+        let sessionId = await getSessionId()
+        let checkoutOptions = {
+          paymentSessionId : sessionId,
+          redirectTarget:"_modal",
+        }
+  
+        cashfree.checkout(checkoutOptions).then((res) => {
+          console.log("payment initialized")
+  
+          verifyPayment(orderId)
+          updatePaymentStatus(orderId)
+        })
+  
+  
+      } catch (error) {
+        console.log(error)
+      }
+  
+    }
   const updatePaymentStatus = async (resId) => {
     // toast.success(resId)
     try {
