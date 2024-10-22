bf8c1776cbcf4bfb4d0dfb9b73ea8e4dd825b9b7
JitenderMX <jitender.work.mediax@gmail.com>
2024-10-22 12:16:12 +0530

    Cashfree back to live mode fix done final

--- backend/app.js
+++ backend/app.js
@@ -42,7 +42,7 @@ app.get('/', (req, res) => {
 })
 
 
-app.get('/payment', async (req, res) => {
+/* app.get('/payment', async (req, res) => {
 
     try {
 
@@ -91,9 +91,60 @@ app.post('/verify', async (req, res) => {
     } catch (error) {
         console.log(error);
     }
-})
+}) */
 
+    app.get('/payment', async (req, res) => {
 
+        try {
+    
+            let request = {
+                "order_amount": 1.00,
+                "order_currency": "INR",
+                "order_id": await generateOrderId(),
+                "customer_details": {
+                    "customer_id": "webcodder01",
+                    "customer_phone": "9999999999",
+                    "customer_name": "Web Codder",
+                    "customer_email": "webcodder@example.com"
+                },
+            }
+    
+            Cashfree.PGCreateOrder("2023-08-01", request).then(response => {
+                console.log(response.data);
+                res.json(response.data);
+    
+            }).catch(error => {
+                console.error(error.response.data.message);
+            })
+    
+    
+        } catch (error) {
+            console.log(error);
+        }
+    
+    
+    })
+    
+    app.post('/verify', async (req, res) => {
+    
+        try {
+    
+            let {
+                orderId
+            } = req.body;
+    
+            Cashfree.PGOrderFetchPayments("2023-08-01", orderId).then((response) => {
+    
+                res.json(response.data);
+            }).catch(error => {
+                console.error(error.response.data.message);
+            })
+    
+    
+        } catch (error) {
+            console.log(error);
+        }
+    })
 app.listen(5000, () => {
     console.log('Server is running on port 5000');
 })
