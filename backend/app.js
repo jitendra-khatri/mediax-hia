264f208ec761b9f9e71a0238193635a8f2c545ba
Jitendra Khatri <jkhatri6@gmail.com>
2024-11-20 19:08:51 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,15 @@ const {
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+
+const corsOptions = {
+    origin: ['https://happening-in-agra.vercel.app', 'https://happeninginagra.com', 'https://mediax-hia.vercel.app'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
